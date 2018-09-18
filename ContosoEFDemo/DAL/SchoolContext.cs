3bc794fab92eedf14fa4a2d81411b33d799404d4
Sun.yuejun <Sun.yuejun@oe.21vianet.com>
2018-09-19 07:05:40 +0800

    x

---
+++ ContosoEFDemo/DAL/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using ContosoEFDemo.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoEFDemo.DAL
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+            Database.SetInitializer<SchoolContext>(null);
+        }
+
+        // 通过DbSet来创建数据库中的Table
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ContosoEFDemo/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoEFDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public int stu_Number { get; set; }
+        public string UpdateDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
